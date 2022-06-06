f3c5b490cd45053d1b35fb40e3b4c8d65b53cf41
dyhmzall <dyhmzall@gmail.com>
2022-06-06 14:35:33 +0300

    feat: dump sql

--- dump.sql
+++ dump.sql
@@ -21,6 +21,9 @@ SET row_security = off;
 --
 
 COPY public.conference (id, city, year, is_international) FROM stdin;
+4	Moscow	2022	t
+5	London	2023	t
+6	Mexico	2024	f
 \.
 
 
@@ -29,6 +32,8 @@ COPY public.conference (id, city, year, is_international) FROM stdin;
 --
 
 COPY public.comment (id, conference_id, author, text, email, create_at, photo_filename) FROM stdin;
+4	4	dyhmzall	very nice!	dyhmzall@gmail.com	2022-06-06 14:32:00	\N
+5	5	Nicklback	bad, very bad	nick@gmail.com	2022-06-03 14:32:00	\N
 \.
 
 
@@ -37,7 +42,7 @@ COPY public.comment (id, conference_id, author, text, email, create_at, photo_fi
 --
 
 COPY public.doctrine_migration_versions (version, executed_at, execution_time) FROM stdin;
-DoctrineMigrations\\Version20220603153043	2022-06-03 15:33:25	78
+DoctrineMigrations\\Version20220606111342	2022-06-06 11:13:53	52
 \.
 
 
@@ -50,17 +55,37 @@ COPY public.messenger_messages (id, body, headers, queue_name, created_at, avail
 
 
 --
+-- Data for Name: status; Type: TABLE DATA; Schema: public; Owner: symfony
+--
+
+COPY public.status (id, title) FROM stdin;
+1	new
+2	active
+3	close
+\.
+
+
+--
+-- Data for Name: task; Type: TABLE DATA; Schema: public; Owner: symfony
+--
+
+COPY public.task (id, status_id, title, url) FROM stdin;
+2	1	Тестовая задача	https://dyhmzall.com/
+\.
+
+
+--
 -- Name: comment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: symfony
 --
 
-SELECT pg_catalog.setval('public.comment_id_seq', 1, false);
+SELECT pg_catalog.setval('public.comment_id_seq', 5, true);
 
 
 --
 -- Name: conference_id_seq; Type: SEQUENCE SET; Schema: public; Owner: symfony
 --
 
-SELECT pg_catalog.setval('public.conference_id_seq', 1, false);
+SELECT pg_catalog.setval('public.conference_id_seq', 6, true);
 
 
 --
@@ -68,6 +93,20 @@ SELECT pg_catalog.setval('public.conference_id_seq', 1, false);
 --
 
 SELECT pg_catalog.setval('public.messenger_messages_id_seq', 1, false);
+
+
+--
+-- Name: status_id_seq; Type: SEQUENCE SET; Schema: public; Owner: symfony
+--
+
+SELECT pg_catalog.setval('public.status_id_seq', 3, true);
+
+
+--
+-- Name: task_id_seq; Type: SEQUENCE SET; Schema: public; Owner: symfony
+--
+
+SELECT pg_catalog.setval('public.task_id_seq', 2, true);
 
 
 --
